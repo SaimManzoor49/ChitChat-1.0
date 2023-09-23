9bc39a78cce89f7241a37c173205364eca035803
unknown <saummanzoor49@gmail.com>
2023-09-23 20:39:03 +0500

    deploy-testing

--- server/index.js
+++ server/index.js
@@ -24,21 +24,21 @@ app.use('/api/message',require('./routes/messageRoutes'));
 
 // Deployment-----------------
 
-const __dirname1 = path.resolve()
-if(process.env.NODE_ENV==='production'){
+// const __dirname1 = path.resolve()
+// if(process.env.NODE_ENV==='production'){
 
-  app.use(express.static(path.join(__dirname1,"..",'/frontend/build')))
+//   app.use(express.static(path.join(__dirname1,"..",'/frontend/build')))
 
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"..","frontend","build","index.html"))
-  })
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname1,"..","frontend","build","index.html"))
+//   })
 
 
-}else{
+// }else{
   app.get("/", (req, res) => {
     res.send("hello world");
   });
-}
+// }
 // Deployment-----------------
 const server = app.listen(PORT, () => {
   console.log(`Server is Listning on Port:${PORT}`);
